f624fb36a11e69785a0934701cd486877cb09f2c
marqnpl <marqnpl@gmail.com>
2016-05-30 21:04:38 +0200

    first commit

---
+++ public/js/controller.js
@@ -0,0 +1,55 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'indexCtrl',
+            templateUrl: 'pages/index.html'
+        })
+        .when('/learn', {
+            controller: 'learnCtrl',
+            templateUrl: 'pages/learn.html'
+        })
+        .when('/word-manager', {
+            controller: 'managerCtrl',
+            templateUrl: 'pages/manager.html'
+        })
+        .when('/login', {
+            controller: 'loginCtrl',
+            templateUrl: 'pages/join.html'
+        })
+        .when('/join', {
+            controller: 'joinCtrl',
+            templateUrl: 'pages/join.html'
+        })
+        .otherwise({
+            template: '<h1>Not Found</h1>'
+        });
+});
+
+app.controller('appCtrl', function ($scope, $location) {
+
+    $scope.pageClass = function (path) {
+        return (path == $location.path()) ? 'active' : '';
+    };
+});
+
+app.controller('indexCtrl', function ($scope) {
+
+});
+
+app.controller('learnCtrl', function ($scope) {
+
+});
+
+app.controller('managerCtrl', function ($scope) {
+    $scope.info = "manager wyrazów";
+});
+
+app.controller('loginCtrl', function ($scope) {
+    $scope.info = "login page";
+});
+
+app.controller('joinCtrl', function ($scope) {
+    $scope.info = "join page";
+});
