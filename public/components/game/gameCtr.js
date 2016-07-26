9e1e185d18881403385d6145d7a70b983eff76c3
marcin.matula <marqnpl@gmail.com>
2016-07-26 16:03:54 +0200

    ...

---
+++ public/components/game/gameCtr.js
@@ -0,0 +1,111 @@
+app.component('gameWindow', {
+    controller: function ($scope, dataAccess, hotkeys, ArrayUtil, gameConfiguration, typeOfGameBtn) {
+
+        $scope.elapsedTime;
+
+        $scope.$on('timer-stopped', function (event, data) {
+            $scope.elapsedTime = data.millis;
+        });
+
+        initGame = function () {
+            $scope.sprawdz = 1; // 1,2,3,4
+            $scope.wordIndex = 0;
+            $scope.wiemCounter = 0;
+            $scope.niewiemCounter = 0;
+            $scope.progress = 0;
+
+            var list = dataAccess.connectArray('words');
+
+            list.$loaded()
+                .then(function () {
+                    $scope.words = ArrayUtil.shuffleArray(list);
+                });
+            $scope.$broadcast('timer-start');
+        };
+
+        $scope.wiem = function () {
+
+            $scope.wiemCounter++;
+
+            if (!$scope.words[$scope.wordIndex].elapsedTime)
+                $scope.words[$scope.wordIndex].elapsedTime = $scope.elapsedTime;
+            else
+                $scope.words[$scope.wordIndex].elapsedTime += $scope.elapsedTime;
+
+            $scope.words[$scope.wordIndex].win++;
+            $scope.words.$save($scope.wordIndex);
+
+            nextWord();
+        };
+
+        $scope.niewiem = function () {
+
+            $scope.niewiemCounter++;
+
+            if (!$scope.words[$scope.wordIndex].elapsedTime)
+                $scope.words[$scope.wordIndex].elapsedTime = $scope.elapsedTime;
+            else
+                $scope.words[$scope.wordIndex].elapsedTime += $scope.elapsedTime;
+
+            $scope.words[$scope.wordIndex].lost++;
+            $scope.words.$save($scope.wordIndex);
+
+            nextWord();
+        };
+
+        $scope.next = function () {
+
+            $scope.$broadcast('timer-stop');
+
+            console.log($scope.elapsedTimer);
+            $scope.words[$scope.wordIndex].refresh++;
+            $scope.words.$save($scope.wordIndex);
+
+            $scope.sprawdz++;
+        };
+
+        $scope.startGame = function () {
+            initGame();
+        };
+
+        nextWord = function () {
+            $scope.sprawdz = 1;
+
+            if (gameConfiguration.getWordsLimit() - 1 == $scope.wordIndex) {
+                $scope.sprawdz = 4;
+            }
+            else {
+                $scope.$broadcast('timer-start');
+            }
+            $scope.wordIndex++;
+            $scope.progress = Math.floor(($scope.wordIndex / gameConfiguration.getWordsLimit()) * 100);
+        };
+
+        hotkeys.bindTo($scope)
+            .add({
+                combo: 'left',
+                callback: function () {
+                    $scope.wiem();
+                }
+            })
+            .add({
+                combo: 'right',
+                callback: function () {
+                    $scope.niewiem();
+                }
+            })
+            .add({
+                combo: 'up',
+                callback: function () {
+                    if ($scope.sprawdz != 4)
+                        $scope.next();
+                    else
+                        $scope.startGame();
+                }
+            });
+
+        initGame();
+    },
+    bindings: {},
+    templateUrl: 'components/game/game.html'
+});
